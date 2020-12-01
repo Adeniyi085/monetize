2e3362e922a739535d07d21bcfd77de3d72fbe24
exit <adeniyi.adegelu@gmail.com>
2020-12-01 07:16:07 +0100

    second commit, Navigation

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -76,11 +76,40 @@ export const NavLinks = styled(LinkS)`
  align-items: center;
  padding: 0 1rem;
  text-decoration: none;
- height:100%;
+ height:100%;   
  cursor: pointer;
    
 
  &.active {
      border-bottom: 3px solid #01bf71;
  }
-`;
+`
+export const NavBtn = styled.nav`
+display: flex;
+align-items: center;
+
+@media screen and (max-width: 768px) {
+    display: none;
+}
+`
+export const NavBtnLink = styled(LinkR)`
+border-radius : 50px;
+background: orange;
+white-space: nowrap;
+padding: 10px 22px;
+color: blue;
+font-size: 16px;
+font-weight: bold;
+outline: none;
+border: none;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+text-decoration: none;
+
+$.hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+}
+`
+
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements'
+import {FaBars} from 'react-icons/fa'
+import {
+    Nav,
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks,
+    NavBtn,
+    NavBtnLink } from './NavbarElements'
 
-const Navbar = () => {
+const Navbar = () => {    
     return (
        <>
         <Nav>
@@ -9,6 +19,26 @@ const Navbar = () => {
                  <NavLogo to="/">
                      Monetize
                  </NavLogo>
+                 <MobileIcon>
+                     <FaBars/>
+                 </MobileIcon>
+                 <NavMenu>
+                     <NavItem>
+                         <NavLinks to='about'>About</NavLinks>
+                     </NavItem>
+                     <NavItem>
+                         <NavLinks to='discover'>Discover</NavLinks>
+                     </NavItem>
+                     <NavItem>
+                         <NavLinks to='services'>Services</NavLinks>
+                     </NavItem>
+                     <NavItem>
+                         <NavLinks to='signup'>Sign Up</NavLinks>
+                     </NavItem>
+                 </NavMenu>
+                 <NavBtn>
+                     <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                 </NavBtn>
             </NavbarContainer>
         </Nav>
        </>
